45803e1d24aa13f07bf8527c4c3b9d6b22e8a9d7
Sophia Kiani <sophkiani@gmail.com>
2021-09-20 23:48:52 -0500

    added js to make the password generator work

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # random-password-generator
+
+
+# Portfolio Profile
+
+    This file contains the basic html and css for the webpage of Sophia Kiani's Portfolio, 
+    include DearSO logo, naviagtion bar, hero, pictures, and project section with links. 
+
+
+# Authors
+    Sophia Kiani 
+    Version 1.1
+
+
+# Screen Shot
+`
+    ![Preview Of Horiseon Home Webpage](./assets/images/sophia_kiani_portfolio_screen_shot.png)
+
+
+    
+# Link
+`
+    [Check out my portfolio webpage...](https://github.com/soph-k/sophia_kiani_portfolio_profile/)
+    [Or check out my  GitHub repository for my portfolio website](https://soph-k.github.io/sophia_kiani_portfolio_profile/) 
+    
+ 
+# Change Log
+
+    HTML: 
+        - Added semantic tag to optimize function of the webpage.
+        - Added alt attributes to for every image.
+        - Removed unnecessary classes.
+        - Added comments to help other developers understand the corresponding css.
+        - Created id's.
+
+    CSS:
+        - Created a table of contents to organize sections for the corresponding css to html sections.
+        - Removed unnecassary repeated css selections.
+
+# Bugs
+
+    There are currently no known bugs within this file, however, it is not a functional webpage yet.
---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+// var charactersGenerated;
+// var randomPassword;
+
+  var passwordLength = prompt("Choose a password that is between 8 and 128 characters long.");
+  var characterLimit = parseInt(passwordLength, 10);
+  if(isNaN(characterLimit) || characterLimit < 8 || characterLimit > 128) {
+    alert("Error! You must choose a passwood that is between 8 and 128 numbers long.");
+      return "Try Again";
+  }
+  else {
+  }
+
+  var upperCaseCharactersQuestion = confirm ("Would you like to have upper case characters in your password?");
+  var lowerCaseCharactersQuestion = confirm ("Would you like to have lower case characters in your password?");
+  var numericalCharactersQuestion = confirm ("Would you like to have you want numbers in your password?");
+  var speicalCharactersQuestion = confirm ("Would you like to have special characters in your password?");
+  if (upperCaseCharactersQuestion && lowerCaseCharactersQuestion && numericalCharactersQuestion && speicalCharactersQuestion) {
+    alert("Use at least one character type.");
+    return "Try Again";
+  }
+  else {
+  }
+  
+  var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseCharacters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numercialCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+  var charactersGenerated
+  if (upperCaseCharactersQuestion) {
+    charactersGenerated = charactersGenerated.concat(upperCaseCharacters);
+  }
+  if (lowerCaseCharactersQuestion) {
+    charactersGenerated = charactersGenerated.concat(lowerCaseCharacters);
+  } 
+  if (numericalCharactersQuestion) {
+    charactersGenerated = charactersGenerated.concat(numercialCharacters);
+  }
+  if (speicalCharactersQuestion) {
+    charactersGenerated = charactersGenerated.concat(specialCharacters);
+  }
+  
+  var finalPassword;
+  for (var i = 0; i < characterLimit; i++) {
+    finalPassword += charactersGenerated[Math.floor(Math.random() * charactersGenerated.length)];
+    return generatePassword();
+  }
+}
+
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
